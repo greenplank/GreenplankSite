2ce7dcdc5ad2e27e20a1c6a83777f7e6b84e099a
greenplank <50463606+greenplank@users.noreply.github.com>
2022-01-10 13:36:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ const HomeTwo = () => {
       <Head>
     <meta name=“google-site-verification” content=“tnWNe69KBE1qlIN2fpG0eEwe-dMpPi5VGPnbL_I6fQ4" />
         <meta name="robots" content="INDEX,FOLLOW" />
-        <meta name="title" content="Miljövanliga komposittrall, fasad, staket och plank" />
-        <meta name="description" content="Green Plank är Europas ledande leverantör av komposittrall, fasadbeklädnad, staket och plank. Skandinavisk kvalitets komposittrall, kompositfasad, staket och plank med minimala underhållskrav för privata och offentliga miljöer." />
+        <meta name="title" content="Miljövanliga komposittrall, fasad, staket och plank med minimalt underhåll och maximal hållbarhet" />
+        <meta name="description" content="Green Plank är Europas ledande leverantör av komposittrall, fasadbeklädnad, staket och plank sedan 2004. Skandinavisk kvalitets komposittrall, kompositfasad, staket och plank med minimala underhållskrav för privata och offentliga miljöer." />
       </Head>
       <Layout pageTitle="Bästa komposittrall | golvplattor | staket och många fler!">
         <HeaderTwo />
