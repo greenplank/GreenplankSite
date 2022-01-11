19bc7dc9057ebb6a24ff20e80739d4345d763549
greenplank <50463606+greenplank@users.noreply.github.com>
2022-01-11 09:42:58 +0100

    Update video-card.js

--- components/videos/video-card.js
+++ components/videos/video-card.js
@@ -27,9 +27,9 @@ const VideoCard = () => {
           Varför Green Plank?
         </h3>
         <h4 style={{color:"white"}}>
-        Varumärket Green Plank är ett hållbart varumärke för byggprodukter som representerar kvalitet, expertis och innovation i mer än 15 år. Green Plank står för innovation, tillförlitlighet, kvalitet, förtroende och kompetens – det här är grunden för vår verksamhet. Vi drivs av en passion för hållbara byggprodukter och detta återspeglas i var och en av våra produkter.
+        Varumärket Green Plank<sup>&reg;</sup> är ett varumärke för hållbara byggprodukter som representerar kvalitet, hållbarhet och innovation i mer än 15 år. Green Plank<sup>&reg;</sup> står för innovation, tillförlitlighet, kvalitet, förtroende och kompetens – det här är grunden för vår verksamhet. Vi drivs av en passion för hållbara byggprodukter och detta återspeglas i var och en av våra kompositprodukter.
         </h4>
-        <h5 style={{color:"white",marginTop:"1em"}}>Som ett paraplymärke omfattar Green Plank en bred varumärkesportfölj i de fyra kategorierna terrasser, fasadbeklädnad, plankor och staket, inklusive produktmärke som AVON Flooring. Green Plank är det perfekta valet för krävande applikationer som hotellplattformar, liksom andra allmänna utrymmen som parker, förfriskningsbarer, verandor och klubbhusplattformar. Green Plank kompositplattor är hållbara, barfota vänliga och lätta att hålla i utmärkt skick år efter år.</h5>
+        <h5 style={{color:"white",marginTop:"1em"}}>Som ett paraplymärke omfattar Green Plank en bred varumärkesportfölj i de fyra kategorierna komposit terrasser, komposit fasadbeklädnad, kompositplankor och staket, inklusive produktmärke som AVON Flooring<sup>&reg;</sup>. Green Plank<sup>&reg;</sup> är det perfekta valet för krävande applikationer som hotellplattformar, liksom andra allmänna utrymmen som parker, förfriskningsbarer, brygger, verandor och klubbhusplattformar. Green Plank<sup>&reg;</sup> komposittrall är hållbara, barfota vänliga och lätta att hålla i utmärkt skick år efter år.</h5>
         <div className="video-card__btn-block">
           <Link href="/bestall-prov">
             <a className="thm-btn dynamic-radius">Beställ prov</a>
